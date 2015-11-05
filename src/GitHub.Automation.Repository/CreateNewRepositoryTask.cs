0b67dcc92b6f3e26769e7d06a4fe127a64f92d14
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-05 22:58:54 +0100

    Create new repository. Fixes #3

---
+++ src/GitHub.Automation.Repository/CreateNewRepositoryTask.cs
@@ -0,0 +1,28 @@
+﻿using GitHub.Automation.Configuration;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace GitHub.Automation.Repository
+{
+    public class CreateNewRepositoryTask
+    {
+        private IGitHubClient Client { get; }
+        private NewRepositoryConfiguration Configuration { get; }
+
+        public CreateNewRepositoryTask(IGitHubClient client, NewRepositoryConfiguration configuration)
+        {
+            Client = client;
+            Configuration = configuration;
+        }
+        public async Task<string> CreateAsync(string name)
+        {
+            var newRepo = new NewRepository(name)
+            {
+                AutoInit = true
+            };
+
+            var repository = await Client.Repository.Create(newRepo);
+            return repository.HtmlUrl;
+        }
+    }
+}
