8cd40407277ac6e61550886775e06de74e81801b
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-05 16:26:38 +0100

    solution created, couple of line of code added

---
+++ src/GitHub.Automation.Configuration/Config.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GitHub.Automation.Configuration
+{
+    public class Branch
+    {
+        public string name { get; set; }
+        public bool @default { get; set; }
+        public bool @protected { get; set; }
+    }
+
+    public class Label
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Config
+    {
+        public List<Branch> branches { get; set; }
+        public List<Label> labels { get; set; }
+    }
+
+    public class NewRepositoryConfiguration
+    {
+        public double version { get; set; }
+        public Config repository { get; set; }
+    }
+}
---
+++ src/GitHub.Automation.Configuration/ConfigurationProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GitHub.Automation.Configuration
+{
+    public class ConfigurationProvider
+    {
+        private const string owner = "owner";
+        private const string repo = "repo";
+
+        public async Task<NewRepositoryConfiguration> GetConfigurationAsync(IGitHubClient client)
+        {
+            var contents =
+                await
+                    client.Repository.Content.GetAllContents(owner, repo, "configuration/configuration.json")
+                        .ConfigureAwait(false);
+            var configurationString = contents.First().Content;
+            //deserialize
+
+            return null;
+        }
+    }
+}
