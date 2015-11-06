37928c6155aa0af3a2424ec639357f3926f09b93
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-06 21:50:14 +0100

    remove Configuration project

---
+++ src/GitHub.Automation.Repository/Configuration/Config.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GitHub.Automation.Configuration
+{
+    public class Branch
+    {
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsProtected { get; set; }
+    }
+
+    public class Label
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+
+    public class Config
+    {
+        public List<Branch> branches { get; set; }
+        public List<Label> labels { get; set; }
+    }
+
+    public class NewRepositoryConfiguration
+    {
+        public double version { get; set; }
+        public Config repository { get; set; }
+    }
+}
